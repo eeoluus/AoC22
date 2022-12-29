c0b48833c2c6960f1bae88d6b51a516dd56b463d
eeoluus <86976278+eeoluus@users.noreply.github.com>
2022-12-29 03:10:17 +0200

    fairly simple

---
+++ SolutionDay6A.py
@@ -0,0 +1,42 @@
+# ### PROBLEM DESCRIPTION
+#
+# A start-of-packet marker is a substring where all characters are 
+# different. Given a string, how many characters have 
+# to be processed until a marker of length 4 is found?
+#
+# More info at https://adventofcode.com/2022/day/6.
+#
+
+def solution(file):
+    """Solve the first problem of Advent of Code 2022 Day 6.
+    
+    Scan the signal with a moving window of length 4. Converting to set 
+    removes duplicates, so compare lengths of window and set of elements
+    in the window. If lengths match, all elements are unique and the
+    solution is found.
+    """
+    
+    signal = file.read()
+    window_size = 4
+    
+    i = 0
+    while i < len(signal) - window_size + 1:
+
+        last_sample_processed = i + window_size
+
+        window = signal[i : last_sample_processed]
+
+        if len(window) == len(set(window)):
+            return last_sample_processed
+
+        i += 1
+    
+    return "Could not find a start-of-packet marker."
+
+if __name__ == '__main__':
+    
+    with open("C:/Users/eerou/Desktop/input.txt") as file:
+        
+        marker_end = solution(file)
+        print(marker_end)
+
